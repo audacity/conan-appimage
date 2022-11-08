cfa1bc54fd9536df2cc834baed36d721a8f0a806
Dmitry Vedenko <dmitry@crsib.me>
2022-11-08 15:16:03 +0300

    Different way to update github environment

--- build.sh
+++ build.sh
@@ -9,7 +9,12 @@ python-appimage build app -p 3.10 -l manylinux2014_x86_64 ./appimage
 version_string=$(./conan-appimage-x86_64.AppImage --version)
 version_string=${version_string#Conan version }
 
-echo "Conan version: ${version_string}"
-
 mv conan-appimage-x86_64.AppImage conan-${version_string}-x86_64.AppImage
-echo "conan_version=${version_string}" >> $GITHUB_ENV
+
+function gh_export()
+{
+    [[ "${GITHUB_ENV-}" ]] || local -r GITHUB_ENV="/dev/null"
+    export -- "$@" && printf "%s\n" "$@" >> "${GITHUB_ENV}"
+}
+
+gh_export CONAN_VERSION="${version_string}"
